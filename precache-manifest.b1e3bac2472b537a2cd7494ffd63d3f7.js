bfed515fd02add6be57d8314fd63f741197af900
Nicolas Abitante <nicolas.abitante@ripio.com>
2023-06-05 19:41:34 -0300

    Updates

---
+++ precache-manifest.b1e3bac2472b537a2cd7494ffd63d3f7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6665917a3f871f219279",
+    "url": "/ibher/static/css/main.8d50cac9.chunk.css"
+  },
+  {
+    "revision": "6665917a3f871f219279",
+    "url": "/ibher/static/js/main.6665917a.chunk.js"
+  },
+  {
+    "revision": "74c14461ced71b9073e9",
+    "url": "/ibher/static/css/1.dba5b00e.chunk.css"
+  },
+  {
+    "revision": "74c14461ced71b9073e9",
+    "url": "/ibher/static/js/1.74c14461.chunk.js"
+  },
+  {
+    "revision": "9ab461fe77bf36ce6fd9",
+    "url": "/ibher/static/js/runtime~main.9ab461fe.js"
+  },
+  {
+    "revision": "9e1be994582b832aa5a1a4d74b31a86a",
+    "url": "/ibher/static/media/Loading.9e1be994.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ibher/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ibher/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ibher/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ibher/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "91db5cc47d3baf25afd5deb54d404447",
+    "url": "/ibher/index.html"
+  }
+];
